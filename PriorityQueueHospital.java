fa3bae238d15b89add49a4cdd3f5fc0cf2243565
Lowry West <4lowrywest@gmail.com>
2019-03-07 11:04:19 -0600

    Extending Abstract classes and implementing methods

--- PriorityQueueHospital.java
+++ PriorityQueueHospital.java
@@ -1,4 +1,44 @@
 
-public class PriorityQueueHospital {
+public class PriorityQueueHospital extends Hospital{
+
+	public PriorityQueueHospital()
+	{
+		
+	}
+	@Override
+	public void addPatient(Object patient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object nextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object treatNextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numPatients() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String hospitalType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String allPatientInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- StackHospital.java
+++ StackHospital.java
@@ -1,4 +1,45 @@
 
-public class StackHospital {
+public class StackHospital<PatientType> extends Hospital {
+	
+	public StackHospital()
+	{
+		
+	}
+
+	@Override
+	public void addPatient(Object patient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object nextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object treatNextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numPatients() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String hospitalType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String allPatientInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

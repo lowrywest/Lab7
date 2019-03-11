6f1bc03ca467f661373b0c69d574788f6f53a6f4
Lowry West <4lowrywest@gmail.com>
2019-03-11 11:39:00 -0500

    SickPerson Finished

--- SickPerson.java
+++ SickPerson.java
@@ -2,6 +2,13 @@
 public class SickPerson extends Person {
 
 	private int severity;
+	
+	public SickPerson(String name, int age, int severity) 
+	{
+		super(name, age);
+		this.severity=severity;
+		// TODO Auto-generated constructor stub
+	}
 	/**
 	 * @return the severity
 	 */
@@ -10,17 +17,35 @@ public class SickPerson extends Person {
 		return severity;
 	}
 
-	public SickPerson(String name, int age, int severity) 
-	{
-		super(name, age);
-		this.severity=severity;
-		// TODO Auto-generated constructor stub
-	}
 
 	@Override
-	protected int compareToImpl(Person p) {
+	protected int compareToImpl(Person p) 
+	{
+		if(!p.toString().equalsIgnoreCase(this.toString()))
+		{
+			return 0;
+		}
+		else
+		{
+			if(((SickPerson) p).getSeverity()<severity)
+			{
+				return -1;
+			}
+			else if(((SickPerson) p).getSeverity()>severity)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
 		// TODO Auto-generated method stub
-		return 0;
+	}
+	
+	public String toString()
+	{
+		return String.format("%s Severity level %d", super.toString(),severity);
 	}
 
 }
